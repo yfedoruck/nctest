ab1f5f5b00d11bcb0333dbf01ed59750f635a4e0
Fedoruk Yaroslav <y.fedoruk@favbet.com>
2018-09-19 10:48:44 +0300

    --

--- ncur.cpp
+++ ncur.cpp
@@ -64,8 +64,9 @@ int main ()
     // ++++++++++++++++++ PARSE DOM
     for (SizeType i = 0; i < issues.Size(); i++) {
         const Value &issue = issues[i];
-        cell(issue["key"].GetString(), 3, 15, 1 + i * 3, 2);
-        cell(issue["fields"]["summary"].GetString(), 3, 30, 1+i*3, 20);
+        //cell(issue["key"].GetString(), 3, 15, 1 + i * 3, 2);
+        //cell(issue["fields"]["summary"].GetString(), 3, 30, 1+i*3, 20);
+        cell(issue["key"].GetString(), 3, 30, 1+i*3, 20);
         //mvprintw(12 + 2*i + 1, 10, issue["fields"]["summary"].GetString());
 
         //mvprintw(12 + i, 10, issue["key"].GetString());
